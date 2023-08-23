8641873e970130798bfb0a977e4768c3563aaa6f
effiecancode <faitheffie25@gmail.com>
2023-08-23 13:20:26 +0300

    Init sort

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates a storage object"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 from os import getenv
 
 
