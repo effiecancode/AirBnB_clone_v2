78efc86db0fe2a5f60b0b0e7332a60cd5f63c0d4
effiecancode <faitheffie25@gmail.com>
2023-08-23 10:49:46 +0300

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -14,8 +14,8 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
-    # if getenv("HBNB_MYSQL_DB") == "db":
-    cities = relationship("City", backref='state',
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref='state',
                           cascade='all, delete, delete-orphan')
 
     @property
