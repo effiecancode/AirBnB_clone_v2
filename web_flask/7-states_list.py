f4b13c4e62aff6f9d3c9c629141427dad5cf2d4e
effiecancode <faitheffie25@gmail.com>
2023-09-24 12:06:04 +0300

    states list fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,9 +4,11 @@ Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from models import storage
+from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
@@ -14,13 +16,15 @@ def states_list():
     Displays a html page with a list of all states objects from dbstorgae
     sorted by name
     """
-    states = storage.all("state")
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
+
 @app.teardown_appcontext
-def teardown(exc):
+def teardown_session(exc):
     """Remove the current SQLAlchemy session"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
